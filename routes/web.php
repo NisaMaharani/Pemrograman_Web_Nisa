40de691b46f62c46393a47b0a17cc058b254baf2
NisaMaharani <nisamaharani2020@gmail.com>
2024-09-02 10:53:26 +0700

    Edit Delete dan Validasi Mahasiswa

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/mahasiswa', [MahasiswaController::class, 'index'])->middleware('auth');
+Route::get('/create', [MahasiswaController::class, 'create']);
+Route::post('/store', [MahasiswaController::class, 'store']);
+Route::get('/edit/{mahasiswa}', [MahasiswaController::class, 'edit']);
+Route::put('/update/{mahasiswa}', [MahasiswaController::class, 'update']);
+Route::delete('/delete/{mahasiswa}', [MahasiswaController::class, 'delete']);
 Route::get('/profile', [ProfileController::class, 'index'])->middleware('auth');
